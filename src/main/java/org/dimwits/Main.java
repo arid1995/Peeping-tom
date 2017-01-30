91c3e3ceff854a55b483c5e4f45cdd25dcbfcbed
arid1995 <arid1995@mail.ru>
2017-01-30 17:58:12 +0300

    initial commit

---
+++ src/main/java/org/dimwits/Main.java
@@ -0,0 +1,10 @@
+package org.dimwits;
+
+/**
+ * Created by farid on 1/30/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("shit, it works");
+    }
+}
